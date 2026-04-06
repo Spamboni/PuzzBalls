e19370c7d956c35f93270807d18b03cc5fab1d4c
Sam <102945038+Spamboni@users.noreply.github.com>
2026-04-06 05:12:15 -0500

    Deploy puzzballs-v14.78 via PuzzBalls Deploy Tool

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['game.js'] = 1477;
+window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['game.js'] = 1478;
 // game.js — PuzzBalls game controller
 
 var SLING_MIN_OFFSET = 10;
