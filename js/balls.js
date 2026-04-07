cb14c975100d2f32e8c34309518ac41dccd935b3
Sam <102945038+Spamboni@users.noreply.github.com>
2026-04-07 04:58:41 -0500

    Deploy puzzballs-v15.27 via PuzzBalls Deploy Tool

--- js/balls.js
+++ js/balls.js
@@ -1,4 +1,4 @@
-window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['balls.js'] = 1526;
+window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['balls.js'] = 1527;
 // balls.js — Ball type definitions and behaviors
 
 var BALL_TYPES = {
