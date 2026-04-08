d527d6384d21fb76ab87666a30ea28a672435965
Sam <102945038+Spamboni@users.noreply.github.com>
2026-04-08 01:45:42 -0500

    Deploy puzzballs-v15.34 via PuzzBalls Deploy Tool

--- js/objects.js
+++ js/objects.js
@@ -1,4 +1,4 @@
-window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['objects.js'] = 1532;
+window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['objects.js'] = 1534;
 /**
  * objects.js
  * Game entity classes.  Each class knows how to draw itself and nothing else.
