4f2459bdf2737f790039b5d0ce420b88ab0b5af1
Sam <102945038+Spamboni@users.noreply.github.com>
2026-04-09 04:07:48 -0500

    Deploy puzzballs-v15_80 via PuzzBalls Deploy Tool

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['ui.js'] = 1579;
+window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['ui.js'] = 1580;
 // ui.js — PuzzBalls in-game HUD + settings with preset system
 
 class UI {
