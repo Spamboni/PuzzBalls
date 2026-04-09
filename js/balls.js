3cd64d245cee312497e906865410eee8998e6501
Sam <102945038+Spamboni@users.noreply.github.com>
2026-04-08 23:25:29 -0500

    Deploy puzzballs-v15_64 via PuzzBalls Deploy Tool

--- js/balls.js
+++ js/balls.js
@@ -1,4 +1,4 @@
-window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['balls.js'] = 1563;
+window.PUZZBALLS_FILE_VERSION = window.PUZZBALLS_FILE_VERSION || {}; window.PUZZBALLS_FILE_VERSION['balls.js'] = 1564;
 // balls.js — Ball type definitions and behaviors
 
 var BALL_TYPES = {
